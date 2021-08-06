21f1ae263da4703b155d0733901c339236c799cd
amandalaskowski <amanda.2002@aol.com>
2021-08-06 20:38:46 +0000

    just needs api & button

--- src/js/component/todoList.js
+++ src/js/component/todoList.js
@@ -5,32 +5,47 @@ export function TodoList() {
 	const [userInput, setUserInput] = React.useState("");
 
 	return (
-		<div className="maindiv">
-			<input
-				placeholder="What needs to be done?"
-				type="text"
-				value={userInput}
-				onChange={event => {
-					setUserInput(event.target.value);
-				}}
-				onKeyPress={event => {
-					if (event.key === "Enter") {
-						const newTodoList = todoList.concat([userInput]);
-						setTodoList(newTodoList);
-						setUserInput("");
-					}
-				}}></input>
-			<ul>
-				{todoList.map((item, index) => {
-					return (
-						<li key={index}>
-							{item}
-							<button>X</button>
-						</li>
-					);
-				})}
-				<li className="itemsLeft">{todoList.length} items left</li>
-			</ul>
+		<div className="body">
+			<h1>todos</h1>
+			<div className="boxShadow">
+				<input
+					placeholder="What needs to be done?"
+					type="text"
+					value={userInput}
+					onChange={event => {
+						setUserInput(event.target.value);
+					}}
+					onKeyPress={event => {
+						if (event.key === "Enter") {
+							const newTodoList = todoList.concat([userInput]);
+							setTodoList(newTodoList);
+							setUserInput("");
+						}
+					}}></input>
+				<ul>
+					{todoList.map((item, index) => {
+						return (
+							<div className="buttonAppear" key={index}>
+								<li className="item" key={index}>
+									{item}
+									<button className="button1">X</button>
+								</li>
+							</div>
+						);
+					})}
+					<li className="itemsLeft">
+						{todoList.length} items left
+						<button
+							className="button2"
+							id="clearAll"
+							onClick={() => {
+								setTodoList([]);
+							}}>
+							clear all
+						</button>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
